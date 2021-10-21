f819938a96e60351cc1c7464f2418c0bb5c543bb
Galaxy-Wizard <kozlov_sergey_aleksandrovitch@mail.ru>
2021-10-21 10:38:17 +0300

    Resources.

--- GalaxySystemsChat/resource.h
+++ GalaxySystemsChat/resource.h
@@ -1,6 +1,6 @@
 ﻿//{{NO_DEPENDENCIES}}
-// Включаемый файл, созданный в Microsoft Visual C++.
-// Используется GalaxySystemsChat.rc
+// Microsoft Visual C++ generated include file.
+// Used by GalaxySystemsChat.rc
 //
 #define IDM_ABOUTBOX                    0x0010
 #define IDD_ABOUTBOX                    100
@@ -9,6 +9,7 @@
 #define IDD_GALAXYSYSTEMSCHAT_DIALOG    102
 #define IDP_SOCKETS_INIT_FAILED         103
 #define IDR_MAINFRAME                   128
+#define IDD_DIALOG_TAB_PAGE             130
 #define IDC_BUTTON1                     1000
 #define IDC_COMBO1                      1001
 #define IDC_BUTTON2                     1002
@@ -19,14 +20,34 @@
 #define IDC_BUTTON5                     1007
 #define IDC_EDIT2                       1008
 #define IDC_COMBO3                      1009
+#define IDC_BUTTON6                     1010
+#define IDC_EDIT3                       1011
+#define IDC_CHECK1                      1014
+#define IDC_CHECK2                      1015
+#define IDC_EDIT4                       1016
+#define IDC_CHECK3                      1017
+#define IDC_CHECK4                      1018
+#define IDC_RADIO1                      1019
+#define IDC_RADIO2                      1020
+#define IDC_BUTTON7                     1021
+#define IDC_BUTTON8                     1023
+#define IDC_BUTTON9                     1025
+#define IDC_BUTTON10                    1026
+#define IDC_BUTTON11                    1027
+#define IDC_BUTTON12                    1028
+#define IDC_TAB1                        1029
+#define IDC_COMBO4                      1030
+#define IDC_BUTTON13                    1031
+#define IDC_STATIC_TAB                  1032
+#define IDC_STATIC_TAB_BASE             1033
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_CONTROL_VALUE         1034
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
