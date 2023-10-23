48d6763d19cf08ef8fc7e92e3d46484dbe43b533
Galaxy-Wizard <KSA811227@OUTLOOK.COM>
2023-10-23 17:51:03 +0300

    Resources.

--- GalaxySystemsChat/resource.h
+++ GalaxySystemsChat/resource.h
@@ -46,6 +46,7 @@
 #define IDC_RADIO3                      1034
 #define IDC_RADIO4                      1035
 #define IDC_EDIT8                       1036
+#define IDC_CHECK5                      1037
 
 // Next default values for new objects
 // 
@@ -53,7 +54,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1037
+#define _APS_NEXT_CONTROL_VALUE         1038
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
